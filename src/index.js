168c3b60b363c5f62b2d6b095a7f1147eeb1ae6e
powellmad <madsnpowell@gmail.com>
2021-06-15 20:06:34 -0500

    made a temporary landing page with a logout button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Shelf } from './components/Shelf.js';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Shelf />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
